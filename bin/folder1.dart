636cf6d377f62076b159d0275c32d8d26a2cd614
AlvinHartono <128015193+AlvinHartono@users.noreply.github.com>
2023-09-15 10:24:17 +0700

    coba commit

---
+++ bin/folder1.dart
@@ -0,0 +1,7 @@
+import 'package:folder1/folder1.dart';
+
+void main() {
+  for (int i = 0; i < 10; i++) {
+    print('Hello world: ${Calculate().name()}');
+  }
+}
---
+++ lib/folder1.dart
@@ -0,0 +1,9 @@
+class Calculate {
+  int calculate() {
+    return 9 * 8;
+  }
+
+  String name() {
+    return "Alvin";
+  }
+}
